5ce67fca8ce8cc153594d951bc61716efadb4f0f
CarloLonghi <carlo.longhi98@gmail.com>
2025-09-23 16:47:46 +0200

    multiprocessing

--- 4_cl_experiments.py
+++ 4_cl_experiments.py
@@ -2,55 +2,60 @@ import numpy as np
 import evoEGT as evo
 from heterogeneous4 import calcH, calcWCD
 import os
+import concurrent
+import multiprocessing as mp
 
-def coop_pF_r(rv,M,N,HZ,beta,eps,pSv,deltaLv,f,betaF,file,backup):
-# Input: pFv, rv, Mv (vectors with values of pF, r, and M), N, HZ (H or Z), beta, eps
-# Output: matrix with the fraction of cooperators as a function of pF and r
-    if np.isscalar(HZ):
-        H=calcH(N-1,HZ-1)
+from ctypes import c_double
 
-    MAT = np.zeros((len(rv), len(deltaLv), len(pSv), 4))
+class SharedArrayManager:
+    def __init__(self, n):
+        self.n = n
+        self.shared_array = mp.Array(c_double, len(rv)*len(deltaLv)*len(pSv)*4)
+        self.shape = (MAT.shape)
+    
+    def get_array(self):
+        return np.frombuffer(self.shared_array.get_obj()).reshape(self.shape)
 
-    deltaLv = np.array(deltaLv)
+def worker_function(iddl):
+    """
+    Worker function that performs calculations
+    """
+    result_array = shared_array_manager.get_array()
 
-    if os.path.isfile(backup):
-        with open(folder + backup, "r") as f:
-            content = f.read().strip().split(',')
-        params = list(map(int, content))
-    else:
-        params = [0, 0]
+    deltaL = deltaLv[iddl]
+    deltaF = deltaL
+    for idps in range(0, pSv.shape[0]):
+        pS = pSv[idps]
 
-    if os.path.isfile(file):
-        MAT = np.load(file)
-    else:
-        MAT = np.zeros((len(rv), len(deltaLv), len(pSv), 4))
-        np.save(file, MAT)
+        pF=np.zeros((2,2))
+        pF[0,0] = 1/(1+np.exp(-betaF*(f)))
+        pF[1,1] = 1/(1+np.exp(-betaF*(f)))
+        pF[0,1] = 1/(1+np.exp(-betaF*(f+deltaF)))
+        pF[1,0] = 1/(1+np.exp(-betaF*(f-deltaF)))   
 
-
-    for iddl in range(params[0], deltaLv.shape[0]):
-        deltaL = deltaLv[iddl]
-        deltaF = deltaL
-        for idps in range(params[1] if iddl == params[0] else 0, pSv.shape[0]):
-            pS = pSv[idps]
-
-            pF=np.zeros((2,2))
-            pF[0,0] = 1/(1+np.exp(-betaF*(f)))
-            pF[1,1] = 1/(1+np.exp(-betaF*(f)))
-            pF[0,1] = 1/(1+np.exp(-betaF*(f+deltaF)))
-            pF[1,0] = 1/(1+np.exp(-betaF*(f-deltaF)))   
-
-            WCD=calcWCD(N,eps,pF,deltaL,pS,M)
-            print(deltaL, pS)
+        WCD=calcWCD(N,eps,pF,deltaL,pS,M)
+        print(deltaL, pS)
+        
+        for idr, r, in enumerate(rv):
+            SD,fixM = evo.Wgroup2SD(WCD,H,[r,-1.],beta,infocheck=False)
             
-            for idr, r, in enumerate(rv):
-                SD,fixM = evo.Wgroup2SD(WCD,H,[r,-1.],beta,infocheck=False)
-                MAT[idr, iddl, idps] = SD[:,0]
+            result_array[idr, iddl, idps] = SD[:,0]            
 
-            np.save(file, MAT)
-            with open(folder + backup, "w") as f:
-                f.write(f"{iddl},{idps}")
+def coop_pF_r():
+    
+    # Use ProcessPoolExecutor for parallel execution
+    with concurrent.futures.ProcessPoolExecutor(max_workers=mp.cpu_count()) as executor:
+        # Submit all tasks
+        futures = [executor.submit(worker_function, i) for i in range(0, deltaLv.shape[0])]
+        
+        # Wait for all tasks to complete
+        for future in futures:
+            future.result()
+    
+    # Get final results
+    final_result = shared_array_manager.get_array()
+    return final_result
 
-    return MAT
 
 def plotCOOPheat(MAT,f1v,f2v,sv,label):
 # Input: MAT (matrix from "coop_pF_r" function), pFv, rv ,Mv (vectors with values of pF, r, and M), label (name for the output file)
@@ -122,21 +127,31 @@ if __name__ == "__main__":
 ####### Plot heatmap #########################################
     eps=0.01 #0.01
     Z=100
-    k=4
+    k=1
     N=9*k
     beta=1.
     M=0
     f=0
     betaF=1
 
-    deltaLv=[0, 1, 2, 4, 8]
+    H=calcH(N-1,Z-1)
+
+    deltaLv=np.array([0, 1, 2, 4, 8])
     pSv=np.linspace(0,1.,num=50)
     rv=[3*k, 6*k, 8*k]
     
+    MAT = np.zeros((len(rv), len(deltaLv), len(pSv), 4))
+    # Create shared array manager
+    n = 5
+    shared_array_manager = SharedArrayManager(n)
+    # Initialize array with zeros
+    result_array = shared_array_manager.get_array()
+    result_array[:] = 0    
+
     folder = './newtests/2bits/strengthstrat/multileader/'
     labfilenpy= folder + 'res_k4'
     backup = folder + 'backup_k4'
-    MAT=coop_pF_r(rv,M,N,Z,beta,eps,pSv,deltaLv,f,betaF,labfilenpy,backup)
+    MAT = coop_pF_r()
     np.save(labfilenpy,MAT)             # save matrix for heatmap
     print('data saved to file!')
     
